d1e52cddc3dc6e457ac9a5cad58cef8f6304b232
Lucas Felipe Felicio Santos <lucasfelicio378@gmail.com>
2022-12-05 00:58:29 -0300

    insert getBookingById

---
+++ src/repositories/booking-repository/index.ts
@@ -0,0 +1,61 @@
+import { prisma } from "@/config";
+import { Ticket, TicketStatus } from "@prisma/client";
+
+async function getBooking(userId: number) {
+  return prisma.booking.findMany({
+    where: {
+      userId,
+    },
+    include: {
+      Room: true,
+    }
+  });
+}
+async function findTickeWithTypeById(ticketId: number) {
+  return prisma.ticket.findFirst({
+    where: {
+      id: ticketId,
+    },
+    include: {
+      TicketType: true,
+    }
+  });
+}
+
+async function findTicketByEnrollmentId(enrollmentId: number) {
+  return prisma.ticket.findFirst({
+    where: {
+      enrollmentId,
+    },
+    include: {
+      TicketType: true, //inner join
+    }
+  });
+}
+
+async function createTicket(ticket: CreateTicketParams) {
+  return prisma.ticket.create({
+    data: {
+      ...ticket,
+    }
+  });
+}
+
+async function ticketProcessPayment(ticketId: number) {
+  return prisma.ticket.update({
+    where: {
+      id: ticketId,
+    },
+    data: {
+      status: TicketStatus.PAID,
+    }
+  });
+}
+
+export type CreateTicketParams = Omit<Ticket, "id" | "createdAt" | "updatedAt">
+
+const bookingRepository = {
+  getBooking,
+};
+
+export default bookingRepository;
---
+++ src/services/booking-service/index.ts
@@ -0,0 +1,30 @@
+import { notFoundError } from "@/errors";
+import bookingRepository from "@/repositories/booking-repository";
+import enrollmentRepository from "@/repositories/enrollment-repository";
+import ticketRepository from "@/repositories/ticket-repository";
+//import { TicketStatus } from "@prisma/client";
+
+async function getBooking(userId: number) {
+  const enrollment = await enrollmentRepository.findWithAddressByUserId(userId);
+  if (!enrollment) {
+    throw notFoundError();
+  }
+  const ticket = await ticketRepository.findTicketByEnrollmentId(enrollment.id);
+
+  if (!ticket || ticket.status === "RESERVED" || ticket.TicketType.isRemote || !ticket.TicketType.includesHotel) {
+    throw notFoundError();
+  }
+
+  const booking = await bookingRepository.getBooking(userId);
+
+  if (!booking) {
+    throw notFoundError();
+  }
+  return booking;
+}
+
+const bookingService = {
+  getBooking
+};
+
+export default bookingService;
